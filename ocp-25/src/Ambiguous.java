64067d250ee3c6f7034e8898deab03ea1f4d879f
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-26 22:23:43 +0100

    add more tests

---
+++ ocp-25/src/Ambiguous.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+//import java.sql.Date;
+//➜  src git:(master) ✗ javac Ambiguous.java
+//        Ambiguous.java:2: error: a type with the same simple name is already defined by the single-type-import of Date
+//import java.sql.Date;
+//^
+//        1 error
+
+public class Ambiguous {
+    static void main(String[] args) {
+
+    }
+}
---
+++ ocp-25/src/PackageDeclaration.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class PackageDeclaration {
+    public static void main() {
+        Random random = new Random();
+        System.out.println(random.nextDouble());
+    }
+}

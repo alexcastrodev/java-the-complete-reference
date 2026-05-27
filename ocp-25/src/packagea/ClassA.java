54c6e89408426fa663ff2cb758c6498e534b0744
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-27 18:53:18 +0100

    add example of page 118 of OCP

--- ocp-25/src/packagea/ClassA.java
+++ ocp-25/src/packagea/ClassA.java
@@ -4,4 +4,11 @@ public class ClassA {
     public static void main(String[] args) {
         System.out.println("ClassA.main()");
     }
+
+    // Instance initializer
+    // become this after compile:
+    //    public ClassA() {
+    //        System.out.println("teste");
+    //    }
+    { System.out.println("teste"); }
 }

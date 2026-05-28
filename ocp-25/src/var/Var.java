c42568617131f05d2b0607dbaa2accbc57d60770
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-28 17:55:13 +0100

    add example of page 145 of OCP

---
+++ ocp-25/src/var/Var.java
@@ -0,0 +1,15 @@
+package var;
+
+public class Var {
+    public void var() {
+        var var = "var";
+    }
+
+    public void Var() {
+        Var var = new Var();
+    }
+
+    public Var() {
+        Var var = new Var();
+    }
+}

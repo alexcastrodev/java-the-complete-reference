ac3ca2cba0ec8dd0861a86158e1998917780dc95
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-28 17:02:42 +0100

    add example of page 143 of OCP

---
+++ ocp-25/src/TypeInference.java
@@ -0,0 +1,12 @@
+public class TypeInference {
+    //    var tricky = "Hello"; // DOES NOT COMPILE
+
+    void breakingSomehthing() {
+        // var silly; // DOES NOT COMPILE
+        var silly = 5;
+    }
+
+    public static void main() {
+
+    }
+}

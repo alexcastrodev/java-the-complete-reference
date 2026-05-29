1830a803d9bc3e0ec5a068406fa96c4f96a62720
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-29 22:30:46 +0100

    add example of page 160 of OCP

--- ocp-25/src/PrimitiveAndReferences.java
+++ ocp-25/src/PrimitiveAndReferences.java
@@ -3,8 +3,11 @@ public class PrimitiveAndReferences {
     // Primitive
     // =========    // Should be assign 0 (zero)
     int a1, a2;
+    float f1;
+    double f2;
     // should be false
     boolean b1, b2;
+    char c1;
 
     // =========
     // Reference
@@ -14,6 +17,9 @@ public class PrimitiveAndReferences {
 
     void main() {
         var out = new PrimitiveAndReferences();
+        IO.println(out.c1);
+        IO.println(out.f1);
+        IO.println(out.f2);
         IO.println(out.a1);
         IO.println(out.a2);
         IO.println(out.b1);

bc485f652a7ac9ca557f21ac339c9b9003cb9a76
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-30 23:24:00 +0100

    add example of page 161 of OCP

--- ocp-25/src/StringBlock.java
+++ ocp-25/src/StringBlock.java
@@ -30,5 +30,17 @@ public class StringBlock {
             """;
 
         IO.println("*" + block + "*");
+
+
+        // MagicData
+        // var magic = 1_329_.0; DOES NOT COMPILE
+//         var magic = 3_1; // COMPILE
+        // var magic = 3_13.0_; DOES NOT COMPILE
+        // var magic = 5_291._2; // DOES NOT COMPILE
+          var magic = 2_234.0_0; // COMPILE
+//        var magic = 9___6; // COMPILE
+        // var magic = _1_3_5_0; // DOES NOT COMPILE
+        //
+        IO.println(magic);
     }
 }

4ef67ba636d795559b4f761ab60ce4ff9511970f
alexcastrodev <alexoliveira7x@gmail.com>
2026-06-04 23:18:25 +0100

    add example of page 195 of OCP

---
+++ ocp-25/src/CompoundAssignment.java
@@ -0,0 +1,10 @@
+public class CompoundAssignment {
+    public static void main(String[] args) {
+        long goat = 10;
+        int sheep = 5;
+//        sheep = sheep * goat; // CompoundAssignment.java:5: error: incompatible types: possible lossy conversion from long to int
+        // WTF Java ?
+        sheep *= goat;
+        System.out.println(sheep);
+    }
+}
